6c6d074c030632d5186d3186775bc2cb1fee03c8
Ranjan Thapa <98253643+sntiago1011@users.noreply.github.com>
2023-07-03 08:59:35 +0545

    key() added to get the keys

--- Hash/implementaion_hash.py
+++ Hash/implementaion_hash.py
@@ -15,10 +15,11 @@ class HashTable:
         found = False
 
         for idx, element in enumerate(self.data[address]):
-
+            print(element)
             if len(element) == 2 and element[0] == key:
                 self.data[address][idx] = (key, val)
                 found = True
+
         if not found:
             self.data[address].append((key, val))
 
@@ -35,15 +36,25 @@ class HashTable:
                 print("delete", idx)
                 del self.data[address][idx]
 
+    def keys(self):
+        if len(self.data) == 0:
+            return None
+        key_array = []
+        for i in range(0, len(self.data)):
+
+            if self.data[i]:
+                if len(self.data[i]) < 1:
+                    key_array.append(self.data[i][0][0])
+
+                for j in range(0, len(self.data[i])):
+                    key_array.append(self.data[i][j][0])
+        return key_array
+
 
 if __name__ == '__main__':
     t = HashTable()
-    t["march 6"] = 310
-    t["march 7"] = 420
-    t["march 8"] = 67
-    t["march 17"] = 63457
-    print(t["march 17"])
-
+    t["Grapes"] = 230
+    t["Apple"] = 230
+    t["Mango"] = 180
     print(t.data)
-    del t["march 17"]
-    print(t.data)
+    print(t.keys())
